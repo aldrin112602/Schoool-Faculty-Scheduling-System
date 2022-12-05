aa7e175ce823d3c72212ec49b3ef7d1b8824b691
Aldrin Caballero <76159577+aldrin112602@users.noreply.github.com>
2022-12-05 21:04:08 +0800

    Delete frontend directory

--- frontend/js/import-html.js
+++
@@ -1,9 +0,0 @@
-// importing html file
-const import_html = (path, destination) => {
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", path);
-  xhr.onload = () => {
-    document.querySelector(destination).innerHTML = xhr.responseText;
-  };
-  xhr.send(null);
-};
